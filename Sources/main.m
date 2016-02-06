a6c98b3545fc124d9617bc80706c81013f6f83fe
Martin Kiss <martin.kiss@me.com>
2016-02-06 10:40:08 +0100

    Create project

---
+++ Sources/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective-Script
+//
+//  Created by Martin Kiss on 6.2.16.
+//  Copyright © 2016 Tricertops. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
